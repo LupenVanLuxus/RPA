f23e8e3458ee28d3bc7fa40291d788c3357020ae
student <student@B23-00>
2018-05-28 11:02:15 +0200

    MVVM

---
+++ MVVMZapisi/MVVMZapisi/ViewModel/MojViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using Windows.UI;
+
+namespace MVVMZapisi
+{
+    class MojViewModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string imeLastnosti = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(imeLastnosti));
+            }
+        }
+        public ObservableCollection<MojZapis> Zapisi { get; set; }
+        private MojZapis trenutni;
+        public MojZapis Trenutni
+        {
+            get { return trenutni; }
+            set { trenutni = value; OnPropertyChanged(); }
+        }
+        public string Naslov { get; set; }
+        public MojViewModel()
+        {
+            Zapisi = new ObservableCollection<MojZapis>();
+            for (int k = 1; k <= 10; k++)
+            {
+                Zapisi.Add(new MojZapis { Ime = k.ToString(), Barva = Colors.Orange });
+            }
+            trenutni = Zapisi.First();
+            Naslov = "Moji zapisi";
+        }
+    }
+}
